5d2ee2c1ef88f51717a9cebaa97b7a79e90b172f
x00098000 <x00098000@gmail.com>
2014-11-06 01:17:12 +0000

    x00098000

---
+++ x00098000-grades.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+case=1
+
+echo Test Case $case
+	case=$((case+1))
+	#List state: {-1, 39, 40, 50, 80, 101 } 
+	echo Inputs: no parameters
+	echo Actual output: 
+		java grades
+	echo Expected output: "Invalid input"
+echo
+echo Test Case $case
+	case=$((case+1))
+	#List state: {-1, 39, 40, 50, 80, 101 } 
+	echo Inputs: 40, 39
+	echo Actual output: $(java grades 40 39)
+	echo Expected output: "Component Fail" 
+echo 
+echo Test Case $case
+	case=$((case+1))
+	#List state: {-1, 39, 40, 50, 80, 101 } 
+	echo Inputs: 40, 40 
+	echo Actual output: $(java grades 40 40)
+	echo Expected output: "Fail"
+echo
+echo Test Case $case
+	case=$((case+1)) 
+	#List state: {-1, 39, 40, 50, 80, 101 } 
+	echo Inputs: 50,50 
+	echo Actual output: $(java grades 50 50)
+	echo Expected output: "Pass"
+echo
+echo Test Case $case
+	case=$((case+1))
+	#List state: {-1, 39, 40, 50, 80, 101 } 
+	echo Inputs: 80,80
+	echo Actual output: $(java grades 80 80)
+	echo Expected output: "Pass with distinction"
+echo
+echo Test Case $case
+	#List state: {-1, 39, 40, 50, 80, 101 } 
+	echo Inputs: -1, 101
+	echo Actual output: $(java grades -1 101)
+	echo Expected output: "Invalid input"
+
