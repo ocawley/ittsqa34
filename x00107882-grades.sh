3c22809ce370c8ee01027c6765f7532d3adb0108
x00107882 <x00107882@gmail.com>
2014-11-06 21:08:52 +0000

    Grades_V2 Author: Krzysztof Trzcinski X00107882

--- x00107882-grades.sh
+++ x00107882-grades.sh
@@ -5,44 +5,44 @@ echo Grades java app - Black Box Test Cases
 	echo
 	echo Test Case 1:
 	echo
-	echo Input: java grades 100 100
+	echo Input: java grades_v2 100 100
 	echo Expected results: "Pass with distinction"
 	echo Actual results:
-	java grades 100 100
+	java grades_v2 100 100
 	echo
         echo Test Case 2:
         echo
-        echo Input: java grades 39 100
+        echo Input: java grades_v2 39 100
         echo Expected results: "Component Fail"
         echo Actual results:
-	java grades 39 100
+	java grades_v2 39 100
         echo
         echo Test Case 3:
         echo
-        echo Input: java grades 80 80
+        echo Input: java grades_v2 80 80
         echo Expected results: "Pass"
         echo Actual results:
-	java grades 80 80
+	java grades_v2 80 80
         echo
         echo Test Case 4:
         echo
-        echo Input: java grades -2 67
+        echo Input: java grades_v2 -2 67
         echo Expected results: "Invalid Input"
         echo Actual results:
-	java grades -2 67
+	java grades_v2 -2 67
         echo
         echo Test Case 5:
         echo
-        echo Input: java grades n/a 20
+        echo Input: java grades_v2 n/a 20
         echo Expected results: "Invalid Input"
         echo Actual results:
-	java grades n/a 20
+	java grades_v2 n/a 20
         echo
         echo Test Case 6:
         echo
-        echo Input: java grades 100
+        echo Input: java_v2 grades 100
         echo Expected results: "Invalid Input"
         echo Actual results:
-	java grades 100
+	java grades_v2 100
         echo
 
